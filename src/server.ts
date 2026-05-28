a1725bab57137191cf15f02e77a21eb3e1c95cad
João Eduardo <blotfinalboss@gmail.com>
2026-05-27 22:27:43 -0300

    eae

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,15 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`deu bom familia http://localhost:${PORT}`);
+});
+
+app.get("efetuarLogin", (req, res) => {
+  res.send("Rota de login funcionando!");
+});
+
 app.get("/", (req, res) => {
   res.send("Servidor Node.js com TypeScript funcionando!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
