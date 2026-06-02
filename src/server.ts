8766d70da10bbf553b7b28cc7076b3629633ab41
goettems <gabrielggoettems@gmail.com>
2026-06-01 22:05:57 -0300

    backend Validacao

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,37 @@
 import express from "express";
 import { usuario } from "./data/Mock";
+import cors from "cors";
 
 const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`deu bom familia http://localhost:${PORT}`);
+
+app.use(cors({
+  origin: "http://localhost:5173",   // seu frontend Vite
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}))
+
+app.use(express.json()) 
+
+
+app.post("/EfetuarCadastro", (req, res) => {
+  const { nome, idade, email, senha, confirmarSenha } = req.body;
+
+  return res.status(200).json({
+    'nome': nome,
+    'idade': idade,
+    'email': email,
+    'senha': senha,
+    'confirmarSenha': confirmarSenha,
+  });
 });
+
 
 app.get("/listarUsuarios/:codigo", (req, res) => {
   const codigo = req.params.codigo;
+  
 
   for (const user of usuario)  {
     if (user.codigo === Number(codigo)) {
