f63e44ba9165eac588ba8ae82f6f9bff17e75fbe
goettems <gabrielggoettems@gmail.com>
2026-06-22 20:28:12 -0300

    Cadastro de produtos e barra de pesquisa

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -2,12 +2,15 @@ import { Request, Response } from "express";
 import { ProdutoService } from "../services/ProdutoService";
 
 const produtoService = new ProdutoService();
-export function getListarProdutos(req: Request, res: Response) {
-  const { pesquisar } = req.query;
 
-  if (pesquisar == null || pesquisar == "") {
+export function getListarProdutos(req: Request, res: Response) {
+  const pesquisar = req.query.pesquisar as string;
+
+  if (!pesquisar?.trim()) {
     return res.status(200).json(produtoService.buscarTodosProdutos());
   }
 
-    return res.status(200).json(produtoService.buscarProdutosPorNome(pesquisar));
+  const produtos = produtoService.buscarProdutosPorNome(pesquisar);
+
+  return res.status(200).json(produtos);
 }
