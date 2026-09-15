2465c462a6f9ea7308a4c1b5ccc4b3ca5fc2b295
goettems <gabrielggoettems@gmail.com>
2026-09-14 21:03:27 -0300

    erro backend arrumado,com reinstalamento do node_modules

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,22 @@ const app = express();
 const PORT = process.env.PORTA ?? 3000;
 
 
-app.use(cors());
+app.use(cors({
+  origin: "localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
+app.use("/login", authRoutes);
 
-app.use(authRoutes);
+app.use("/login", authRoutes);
 app.use("/produto", produtoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando");   
+});
+ 
